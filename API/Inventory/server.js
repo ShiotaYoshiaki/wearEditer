c5518a21b32adf0caa5ce0f1dbeae016513d512c
SickGort <ohayoppe-yoshi@outlook.jp>
2019-08-18 23:31:19 +0900

    api

---
+++ API/Inventory/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import config from './config';
+
+const app = express();
+
+app.get('/api/inventory', (req, res)=>{
+  res.send('Hello express');
+});
+
+//3000番ポートを使ってサーバーを立ち上げ
+app.listen(config.port, ()=>{
+  console.log('app listening on 3000');
+});
