01308c951ffd3fa97f73afb83864914b58f717af
SickGort <ohayoppe-yoshi@outlook.jp>
2019-09-18 22:58:41 +0900

    speedDialを追加

--- front/src/components/common/body.jsx
+++ front/src/components/common/body.jsx
@@ -5,6 +5,7 @@ import Top from '../../containers/top/topIndex';
 import EditTagModal from '../../containers/common/editTagModal';
 import AddContentModal from '../../containers/common/addContentModal';
 import Setting from '../../containers/common/setting';
+import SpeedDial from '../../containers/common/speedDial';
 import { BodyDiv } from '../../style/common/body';
 import { MENU } from '../../constants/parameter';
 
@@ -30,13 +31,14 @@ export default class Body extends React.Component {
   render() {
     return (
       // <BrowserRouter>
-        <BodyDiv>
-          {/* <Route path='/' component={Top} /> */}
-          {this.createBodyContent()}
-          <EditTagModal />
-          <AddContentModal />
-          <Setting />
-        </BodyDiv>
+      <BodyDiv>
+        {/* <Route path='/' component={Top} /> */}
+        {this.createBodyContent()}
+        <SpeedDial />
+        <EditTagModal />
+        <AddContentModal />
+        <Setting />
+      </BodyDiv>
       // </BrowserRouter>
     );
   }
---
+++ front/src/components/common/speedDial.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import SpeedDial from '@material-ui/lab/SpeedDial';
+import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
+import FileCopyIcon from '@material-ui/icons/FileCopyOutlined';
+import SaveIcon from '@material-ui/icons/Save';
+import PrintIcon from '@material-ui/icons/Print';
+import ShareIcon from '@material-ui/icons/Share';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { withStyles } from '@material-ui/styles';
+
+const useStyles = {
+  root: {
+    height: 380,
+  },
+  speedDial: {
+    position: 'fixed',
+    display: 'flex',
+    bottom: '55px',
+    right: 0,
+  },
+};
+
+const actions = [
+  { icon: <FileCopyIcon />, name: 'Copy' },
+  { icon: <SaveIcon />, name: 'Save' },
+  { icon: <PrintIcon />, name: 'Print' },
+  { icon: <ShareIcon />, name: 'Share' },
+  { icon: <DeleteIcon />, name: 'Delete' },
+];
+
+class speedDial extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleClick = () => {
+    const { open: openState } = this.state;
+    this.setState({open : !openState});
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { open } = this.state;
+    return (
+      <div className={classes.root}>
+        <SpeedDial
+          ariaLabel="SpeedDial tooltip example"
+          className={classes.speedDial}
+          icon={<SpeedDialIcon />}
+          onBlur={this.handleClose}
+          onClick={this.handleClick}
+          onClose={this.handleClose}
+          onFocus={this.handleOpen}
+          onMouseEnter={this.handleOpen}
+          onMouseLeave={this.handleClose}
+          open={open}
+        >
+          {actions.map(action => (
+            <SpeedDialAction
+              key={action.name}
+              icon={action.icon}
+              tooltipTitle={action.name}
+              tooltipOpen
+              onClick={this.handleClick}
+            />
+          ))}
+        </SpeedDial>
+      </div>
+    );
+  };
+}
+
+export default withStyles(useStyles)(speedDial);
