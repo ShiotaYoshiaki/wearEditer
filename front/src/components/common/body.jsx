71a2f6b1f0a64464a380ccdb0b3e39cde4d4911d
SickGort <ohayoppe-yoshi@outlook.jp>
2019-09-18 21:53:45 +0900

    addContent→addContentModalにrename

--- front/src/components/common/body.jsx
+++ front/src/components/common/body.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import ItemList from '../../containers/inventory/itemList';
 import Top from '../../containers/top/topIndex';
 import EditTagModal from '../../containers/common/editTagModal';
-import AddContent from '../../containers/common/addContent';
+import AddContentModal from '../../containers/common/addContentModal';
 import Setting from '../../containers/common/setting';
 import { BodyDiv } from '../../style/common/body';
 import { MENU } from '../../constants/parameter';
@@ -34,7 +34,7 @@ export default class Body extends React.Component {
           {/* <Route path='/' component={Top} /> */}
           {this.createBodyContent()}
           <EditTagModal />
-          <AddContent />
+          <AddContentModal />
           <Setting />
         </BodyDiv>
       // </BrowserRouter>
